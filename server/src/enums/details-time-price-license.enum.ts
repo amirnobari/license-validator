6fcc13ed8e03dac086d1139580ce8bea04403f94
amir <amirnobari1990@gmail.com>
2023-05-27 18:14:20 +0330

    create enum file for details-time-price-license

---
+++ server/src/enums/details-time-price-license.enum.ts
@@ -0,0 +1,17 @@
+export enum DetailsLicensePriceEnum{
+    ThreeHundredThousand = '300000',
+  FiveHundredThousand = '500000',
+  SixHundredThousand = '600000',
+  EightHundredThousand = '800000',
+  OneMillion = '1000000',
+  TwoMillion = '2000000',
+  TreeMillion = '3000000',
+}
+export enum DetailsLicenseTimeEnum{
+    TwentyDay ='20-d',
+    OneMonth = '1-m',
+    ThreeMonth = '3-m',
+    SixMonth = '6-m',
+    NineMonth = '9-m',
+    OneYear = '1-y',
+}
